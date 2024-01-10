315ff964b42bdc1c17dbec69d1e356f2d88ab784
BancroftH <harrisonbancroft26@gmail.com>
2024-01-10 10:32:00 -0500

    prompt-work

---
+++ index.js
@@ -0,0 +1,7 @@
+// promts user to enter a list of froyo flavors seperated by commas
+const inputFlavors = prompt (
+    "Please enter frozen yogurt flavors seperated by commas."
+);
+
+// Splits list of flavors seperated by commas
+const stringArray = inputFlavors.split(",");
