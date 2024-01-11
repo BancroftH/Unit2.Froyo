42183d77a17d8ef6d0feec8881aee997ff7e64f1
BancroftH <harrisonbancroft26@gmail.com>
2024-01-11 18:58:46 -0500

    loop-and-object-work

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // promts user to enter a list of froyo flavors seperated by commas
 const inputFlavors = prompt (
-    "Please enter frozen yogurt flavors seperated by commas."
+    "Please enter frozen yogurt flavors seperated by commas.",
+    "Strawberry,Vanilla,Chocolate,Coffee,Blueberry"
 );
 
-// Splits list of flavors seperated by commas
+// Loops through and splits list of flavors seperated by commas
 const stringArray = inputFlavors.split(",");
+for (let i = 0; i < stringArray.length; i++) {
+    console.log(stringArray[i]);
+}
+// An object is used to keep count of how many orders there are of each flavor
+const orders = {
+    Strawberry: 3,
+    Vanilla: 6,
+    Chocolate:2,
+    Coffee: 1,
+    Blueberry: 4,
+}
+console.log(Object.keys(orders));
+console.log(Object.values(orders));
